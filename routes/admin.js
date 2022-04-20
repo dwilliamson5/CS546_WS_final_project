9bb01439a20846c82da8be96bf59a5f401478954
Matt Tarantino <mtarantino925@gmail.com>
2022-04-20 07:24:40 -0400

    admin wip

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,28 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    res.render('admin/index', { title: 'Unisell Admin', universities: [] });
+});
 
+router.get('/new', async (req, res) => {
+    res.render('admin/new', { title: 'New University' });
+});
+
+router.get('/edit/:id', async (req, res) => {
+    // find university. Redirect if it doesnt exist
+    res.render('admin/new', { title: 'Edit University' });
+});
+
+router.post('/', async (req, res) => {
+    // create university, redirect to index once done
+});
+
+router.put('/:id', async (req, res) => {
+    // update university, redirect to index once done
+});
+
+router.delete('/:id', async (req, res) => {
+    // delete university, redirect to index once done
 });
 
 module.exports = router;
