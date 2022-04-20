abe5b47ded829912cacf1ffe23751713c55d9407
Matt Tarantino <mtarantino925@gmail.com>
2022-04-20 06:42:29 -0400

    single quotes

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
 
 app.use('/public', static);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    name: 'AuthCookie',
-    secret: 'some secret string!',
-    resave: false,
-    saveUninitialized: true
-  }));
+  name: 'AuthCookie',
+  secret: 'some secret string!',
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
 app.listen(3000, () => {
-    console.log("We've now got a server!");
-    console.log('Your routes will be running on http://localhost:3000');
+  console.log("We've now got a server!");
+  console.log('Your routes will be running on http://localhost:3000');
 });
