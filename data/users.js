4c96ba3caaf854b80a0321d2093293a5699742d5
Matt Tarantino <mtarantino925@gmail.com>
2022-04-21 18:49:06 -0400

    autoformatting changes

--- data/users.js
+++ data/users.js
@@ -22,16 +22,14 @@ module.exports = {
      * @throws Will throw if parameters are invalid, user already exists,
      *         or there is an issue with the db.
      */
-    async createUser(username, password, name, email, imageURL, bio) 
-    {
+    async createUser(username, password, name, email, imageURL, bio) {
         // Throws if there is an invalid parameter
         validation.isValidUserParameters(username, password, name, email, imageURL, bio);
 
         // Check if username already exists
         const user = await this.getUser(username);
 
-        if (user != null)
-        {
+        if (user != null) {
             throw 'Cannot create username since it already exists!'
         }
 
@@ -43,7 +41,7 @@ module.exports = {
         const SALT_ROUNDS = 16;
         const hash = await bcrypt.hash(password, SALT_ROUNDS);
 
-        let newUser = 
+        let newUser =
         {
             universityId: universityId,
             username: username.trim(),
@@ -60,14 +58,13 @@ module.exports = {
 
         const insertInfo = await userCollection.insertOne(newUser);
 
-        if (!insertInfo.acknowledged || !insertInfo.insertedId)
-        {
+        if (!insertInfo.acknowledged || !insertInfo.insertedId) {
             throw 'Could not add user!';
         }
 
-        return {userInserted: true};
+        return { userInserted: true };
     },
- 
+
     /**
      * Retrieve user from Users collection.
      * 
@@ -75,16 +72,14 @@ module.exports = {
      * @returns The user as an object.
      * @throws Will throw if username parameter is invalid.
      */
-    async getUser(username)
-    {
-        if (!validation.isValidUsername(username))
-        {
+    async getUser(username) {
+        if (!validation.isValidUsername(username)) {
             throw 'Invalid username passed to getUser!'
         }
 
         // Check username as case insensitive
         const usernameRegex = new RegExp(["^", username.trim(), "$"].join(""), "i")
-    
+
         const userCollection = await users();
 
         const user = await userCollection.findOne({ username: usernameRegex });
@@ -101,38 +96,32 @@ module.exports = {
      * @throws Will throw if the parameters are invalid, username doesn't
      *         exist, or the credentials do not match.
      */
-    async checkUser(username, password)
-    {
+    async checkUser(username, password) {
         if (!validation.isValidUsername(username) ||
-            !validation.isValidPassword(password))
-        {
+            !validation.isValidPassword(password)) {
             throw 'Either the username or password is invalid!'
         }
-    
+
         // Get user
         const user = await this.getUser(username);
-        
-        if (user === null) 
-        {
+
+        if (user === null) {
             throw 'Either the username or password is invalid!';
         }
 
         let passwordsMatch = false;
 
-        try
-        {
+        try {
             passwordsMatch = await bcrypt.compare(password, user.password);
         }
-        catch (e) 
-        {
+        catch (e) {
             throw 'Exception occurred when comparing passwords!'
         }
 
-        if (!passwordsMatch)
-        {
+        if (!passwordsMatch) {
             throw 'Either the username or password is invalid!';
         }
 
-        return {authenticated: true};
+        return { authenticated: true };
     }
 }
