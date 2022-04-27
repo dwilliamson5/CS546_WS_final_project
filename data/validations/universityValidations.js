1df11312eb847b9a4ec72089900cc09bd33e903c
Matt Tarantino <mtarantino925@gmail.com>
2022-04-27 09:16:51 -0400

    copied validations

--- data/validations/universityValidations.js
+++ data/validations/universityValidations.js
@@ -1,19 +1,57 @@
-function isValidString(string) {
-  if (
-    string === undefined ||
-    typeof string !== 'string' ||
-    string.trim().length === 0) {
-    return false;
-  }
+const { ObjectId } = require('mongodb');
 
-  return true;
+function objectifyId(id) {
+    return ObjectId(id);
+}
+
+function stringifyId(id) {
+    return id.toString();
+}
+
+function validateObjectId(id) {
+    if (!ObjectId.isValid(id)) {
+        throw 'invalid object ID';
+    }
+}
+
+function checkArgumentLength(args, length) {
+    let argLength = args.length;
+
+    if (argLength > length) {
+        throw `Too many arguments passed to function. Expecting ${length}, got ${argLength}`;
+    } else if (argLength < length) {
+        throw `Too few arguments passed to function. Expecting ${length}, got ${argLength}`;
+    }
+}
+
+function checkParamPresent(variable, variableName) {
+    if (!variable || variable === undefined) {
+        throw `${variableName || 'provided variable'} is not present`;
+    }
+}
+
+function checkIsString(string, variableName) {
+    if (typeof string !== 'string') {
+        throw `${variableName || 'provided variable'} is not a string`;
+    }
+}
+
+function cleanUpString(string) {
+    return string.trim();
+}
+
+function checkStringLength(string, variableName, minLength) {
+    if (string.length === 0) {
+        throw `${variableName || 'provided variable'} is empty or only contains spaces`;
+    }
+
+    minLength = minLength || 1;
+    if (string.length < minLength) {
+        throw `${variableName || 'provided variable'} must be more than ${minLength} characters`;
+    }
 }
 
 function isValidEmail(email) {
-  if (!isValidString(email)) {
-    return false;
-  }
-
   // Check for valid email pattern and only accept .edu addresses
   // Two letter country codes accepted before .edu (e.g. string@string.uk.edu)
   let emailRegex = new RegExp(
@@ -24,14 +62,42 @@ function isValidEmail(email) {
 }
 
 function isValidUniversityParameters(name, emailDomain) {
-  if (!isValidString(name)) throw 'Invalid university name!';
-  if (!isValidEmail(emailDomain)) throw 'Invalid email domain!';
+  checkParamPresent(name);
+  checkParamPresent(emailDomain);
+  
+  checkIsString(name);
+  checkIsString(emailDomain);
 
-  return true;
+  name = cleanUpString(name);
+  emailDomain = cleanUpString(emailDomain);
+
+  checkStringLength(name);
+  checkStringLength(emailDomain);
+
+  if (!isValidEmail(emailDomain)) {
+    throw 'Invalid email domain!';
+  }
+
+  return {
+    name: name,
+    emailDomain: emailDomain
+  };
+}
+
+function isValidUniversityId(id) {
+  checkParamPresent(id);
+  checkIsString(id);
+  id = cleanUpString(id);
+  checkStringLength(id);
+  validateObjectId(id);
+
+  return id;
 }
 
 module.exports = {
-  isValidString,
+  checkArgumentLength,
+  isValidUniversityId,
   isValidEmail,
+  stringifyId,
   isValidUniversityParameters,
 };
