da86bb8363fbd0d9d580138e91abe88e0088ee8c
Matt Tarantino <mtarantino925@gmail.com>
2022-04-29 21:49:39 -0400

    move this back + tweaks

--- data/validations/sharedValidations.js
+++ data/validations/sharedValidations.js
@@ -47,16 +47,6 @@ function checkStringLength(string, variableName, minLength) {
     }
 }
 
-function isValidEmail(email) {
-  // Check for valid email pattern and only accept .edu addresses
-  // Two letter country codes accepted before .edu (e.g. string@string.uk.edu)
-  let emailRegex = new RegExp(
-    "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+(?:[A-Z]{2}|edu)\\b"
-  );
-
-  return emailRegex.test(email);
-}
-
 function isValidUniversityId(id) {
   checkParamPresent(id);
   checkIsString(id);
@@ -75,6 +65,5 @@ module.exports = {
     checkIsString,
     cleanUpString,
     checkStringLength,
-    isValidEmail,
     isValidUniversityId
 };
--- data/validations/universityValidations.js
+++ data/validations/universityValidations.js
@@ -10,12 +10,12 @@ function isValidUniversityParameters(name, emailDomain) {
   name = sharedValidation.cleanUpString(name);
   emailDomain = sharedValidation.cleanUpString(emailDomain);
 
-  emailDomain = emailDomain.toLowerCase();
-
   sharedValidation.checkStringLength(name);
   sharedValidation.checkStringLength(emailDomain);
 
-  if (!sharedValidation.isValidEmail(emailDomain)) {
+  emailDomain = emailDomain.toLowerCase();
+
+  if (!isValidDomain(emailDomain)) {
     throw 'Invalid email domain!';
   }
 
@@ -23,6 +23,16 @@ function isValidUniversityParameters(name, emailDomain) {
     name: name,
     emailDomain: emailDomain
   };
+}
+
+function isValidDomain(email) {
+  // Check for valid email pattern and only accept .edu addresses
+  // Two letter country codes accepted before .edu (e.g. string@string.uk.edu)
+  let emailRegex = new RegExp(
+    "(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+(?:[A-Z]{2}|edu)\\b"
+  );
+
+  return emailRegex.test(email);
 }
 
 module.exports = {
