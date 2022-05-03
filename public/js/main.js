ff4991b64122710af330083d3236ce39f6dfabc1
Matt Tarantino <mtarantino925@gmail.com>
2022-05-03 14:56:51 -0400

    finish up ajax

--- public/js/main.js
+++ public/js/main.js
@@ -4,25 +4,26 @@
 		commentInput = $('#comment'),
 		commentArea = $('#comments-list');
 
-	// commentForm.submit(function(event) {
-	// 	event.preventDefault();
+	commentForm.submit(function(event) {
+		event.preventDefault();
 
-	// 	var commment = commentInput.val();
+		var commment = commentInput.val();
 
-	// 	if (commment) {			
-    //         var requestConfig = {
-    //             method: 'POST',
-    //             url: commentForm.attr('action'),
-    //             contentType: 'application/json',
-    //             data: JSON.stringify({
-    //                 comment: commment
-    //             })
-    //         };
+		if (commment) {			
+            var requestConfig = {
+                method: 'POST',
+                url: commentForm.attr('action'),
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    comment: commment
+                })
+            };
 
-    //         $.ajax(requestConfig).then(function(responseMessage) {
-    //             var newElement = $(responseMessage);
-    //             commentArea.append(newElement);
-    //         });
-	// 	}
-	// });
+            $.ajax(requestConfig).then(function(responseMessage) {
+                var newElement = $(responseMessage);
+                commentArea.append(newElement);
+                commentForm.trigger('reset');
+            });
+		}
+	});
 })(window.jQuery);
