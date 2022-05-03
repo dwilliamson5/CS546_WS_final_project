77946f4644d217933915113199bb34b025f4e344
gpark31 <55361125+gpark31@users.noreply.github.com>
2022-05-03 18:37:56 -0400

    new items routes

--- routes/items.js
+++ routes/items.js
@@ -5,6 +5,7 @@ const items = data.items;
 const users = data.users;
 const itemValidation = require('../data/validations/itemValidations');
 const sharedValidation = require('../data/validations/sharedValidations');
+const xss = require('xss');
 
 router.get('/new', async (req, res) => {
     res.render('items/new', { title: 'New Item' });
@@ -23,6 +24,12 @@ router.post('/', async (req, res) => {
     }
 
     let { item_title, description, keywords, price, photos, pickUpMethod } = body;
+    item_title = xss(item_title);
+    description = xss(description);
+    keywords = xss(keywords);
+    price = xss(price);
+    photos = xss(photos);
+    pickUpMethod = xss(pickUpMethod);
 
     // temp until we get photos working
     photos = 'imag1, image2, image3';
@@ -108,6 +115,7 @@ router.get('/:id', async (req, res) => {
     }
 
     let itemId = params.id;
+    itemId = xss(itemId);
 
     if (!itemId) {
         let user = await users.getUser(req.session.user.username);
@@ -184,13 +192,18 @@ router.get('/:id', async (req, res) => {
         return;
     }
 
+    let comments = await items.getCommentsForItemId(itemId);
+    let bids = await items.getBidsForItemId(itemId, user._id);
+
     res.render('items/show', {
         title: item.title,
         item: item,
         canEdit: req.session.user.username == user.username,
         itemId: itemId,
         user: user,
-        keywords: item.keywords.join(', ')
+        keywords: item.keywords.join(', '),
+        comments: comments,
+        bids: bids
     });
 });
 
@@ -211,6 +224,7 @@ router.get('/:id/edit', async (req, res) => {
     }
 
     let itemId = params.id;
+    itemId = xss(itemId);
 
     if (!itemId) {
         let user = await users.getUser(req.session.user.username);
@@ -328,6 +342,7 @@ router.put('/:id', async (req, res) => {
     }
 
     let itemId = params.id;
+    itemId = xss(itemId);
 
     if (!itemId) {
         let user = await users.getUser(req.session.user.username);
@@ -430,6 +445,13 @@ router.put('/:id', async (req, res) => {
     }
 
     let { item_title, description, keywords, price, photos, pickUpMethod, sold } = body;
+    item_title = xss(item_title);
+    description = xss(description);
+    keywords = xss(keywords);
+    price = xss(price);
+    photos = xss(photos);
+    pickUpMethod = xss(pickUpMethod);
+    sold = xss(sold);
 
     // temp until we have photos
     photos = 'item1, item2, item3';
@@ -508,7 +530,241 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-// create comments
-// create bids
+router.post('/:id/comment', async (req, res) => {
+    let params = req.params;
+
+    if (!params) {
+        let user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'No params provided!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    let itemId = params.id;
+    itemId = xss(itemId);
+
+    if (!itemId) {
+        let user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'No ID param provided!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    let body = req.body;
+
+    if (!body) {
+        let user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'You must supply a body to your request!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    let { comment } = body;
+    comment = xss(comment);
+
+    if (!comment) {
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'You must supply a comment!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    try {
+        itemValidation.isValidComment(itemId, req.session.user.username, comment);
+    } catch (e) {
+        let user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'Your param/body is not vaild!' + e,
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    let item;
+
+    try {
+        item = await items.getItemById(itemId);
+    } catch (e) {
+        let user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'Could not find that item!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    let user;
+
+    try {
+        user = await users.getUserById(item.userId.toString());
+    } catch (e) {
+        let user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'Could not find item owner!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    if (item.universityId.toString() != user.universityId.toString()) {
+        let user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'Cannot view that item because it belongs to another school!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    try {
+        const commentResult = await items.createComment(itemId, req.session.user.username, comment);
+
+        res.render('partials/comment', { layout: null, ...commentResult });
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+ //EDIT HERRE
+router.post('/:id/bid', async (req, res) => {
+    let params = req.params;
+    let user;
+    if (!params) {
+        user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'No params provided!',
+            itemsList: itemsList
+        });
+        return;
+    }
+    let itemId = params.id;
+
+    if (!itemId) {
+        user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'No ID param provided!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    let body = req.body;
+    if (!body) {
+        user = await users.getUser(req.session.user.username);
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'You must supply a body to your request!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    let { bid } = body;
+
+    if (!bid) {
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'You must supply a bid!',
+            itemsList: itemsList
+        });
+        return;
+    }
+    try {
+        user = await users.getUser(req.session.user.username);
+        itemValidation.isValidBid(itemId, bid, user._id);
+    } catch (e) {
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'Your param/body is not vaild!' + e,
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    try {
+        await items.getItemById(itemId);
+    } catch (e) {
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'Could not find that item!',
+            itemsList: itemsList
+        });
+        return;
+    }
+    
+    try {
+        user = await users.getUserById(item.userId.toString());
+    } catch (e) {
+        const itemsList = await items.getAllByUniversityId(user.universityId);
+
+        res.status(404).render('index', {
+            title: 'Item not found',
+            error_status_code: 'HTTP 404 status code',
+            error_messages: 'Could not find item owner!',
+            itemsList: itemsList
+        });
+        return;
+    }
+
+    try {
+        const bidResult = await items.createBids(itemId, bid, user._id);
+
+        res.render('partials/bids', { layout: null, ...bidResult });
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
 
 module.exports = router;
