ee912a1669292b76b65229414bcce9aa992085f3
Matt Tarantino <mtarantino925@gmail.com>
2022-05-03 21:42:41 -0400

    use connect-flash

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars');
 const configRoutes = require('./routes');
 const data = require('./data/index');
 const users = data.users;
+const flash = require('connect-flash');
 
 app.use('/public', static);
 app.use(express.json());
@@ -17,6 +18,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }));
+
+app.use(flash());
 
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
   // If the user posts to the server with a property called _method, rewrite the request's method
