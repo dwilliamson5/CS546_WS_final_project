28729ede1dbfe3aa395878fdda25390f8a13b027
Matt Tarantino <mtarantino925@gmail.com>
2022-05-05 17:30:35 -0400

    unique header photo name

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use('*', async (req, res, next) => {
       if (user.super_admin) {
         res.locals.superAdmin = true;
       }
-      res.locals.imageUrl = user.profileImageUrl || '/public/images/blank.jpg'
+      res.locals.headerImageUrl = user.profileImageUrl || '/public/images/blank.jpg'
     } catch (e) {
       next();
       return;
